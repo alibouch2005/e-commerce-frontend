316a89eadcd941048fb026069ec717a8b2edae88
Ali <ali@gmail.com>
2026-03-11 12:37:09 +0000

    Connexion du frontend React avec l'API Laravel pour tester les produits

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-
+import { useEffect } from "react";
+import api from "./Api/axios";
 function App() {
- 
+
+  useEffect(() => {
+
+    api.get("/api/products?page=1")
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }, []);
 
   return (
-    <>
-      <div>
-        <h1 className='p-5 m-5 text-red-500 bg-blue-900' >Welcome to Vite + React</h1>
-        <input type="text" name="" id="" />
-      </div>
-     
-      
-    </>
-  )
+    <h1 >Frontend React connecté à Laravel</h1>
+  );
 }
 
-export default App
+export default App;
