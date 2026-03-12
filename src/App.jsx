039c85c279c1ae3636414a8471aab94034d4cb3b
Ali <ali@gmail.com>
2026-03-12 21:25:30 +0000

    Ajout de la route Register dans le router principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Products from "./pages/Products";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import Register from "./pages/Register";
 // import Cart from "./pages/Cart";
 
 function App(){
@@ -15,12 +16,12 @@ function App(){
 
         <Route path="/" element={<Home />} />
 
-        {/* <Route path="/login" element={<Login />} />
+         <Route path="/login" element={<Login />} />
 
-        <Route path="/products" element={<Products />} />
+       <Route path="/products" element={<Products />} />
 
-        <Route path="/cart" element={<Cart />} /> */}
-
+        {/* <Route path="/cart" element={<Cart />} /> */}
+        <Route path="/register" element={<Register />} />
       </Routes>
 
     </BrowserRouter>
