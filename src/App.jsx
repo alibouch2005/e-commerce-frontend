06fc43b8d9100d4cc56a06399c586d68c3e13697
Ali <ali@gmail.com>
2026-03-12 22:02:35 +0000

    Ajout de la Navbar globale dans l'application React

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Products from "./pages/Products";
 import Register from "./pages/Register";
+import Navbar from "./components/layout/Navbar";
 // import Cart from "./pages/Cart";
 
-function App(){
-
-  return(
-
+function App() {
+  return (
     <BrowserRouter>
-
+      <Navbar />
       <Routes>
-
         <Route path="/" element={<Home />} />
 
-         <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-       <Route path="/products" element={<Products />} />
+        <Route path="/products" element={<Products />} />
 
         {/* <Route path="/cart" element={<Cart />} /> */}
         <Route path="/register" element={<Register />} />
       </Routes>
-
     </BrowserRouter>
-
-  )
-
+  );
 }
 
 export default App;
