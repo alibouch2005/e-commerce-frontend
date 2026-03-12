c91119fc8aa4e8b71732b79cf77c065d6df6efaf
Ali <ali@gmail.com>
2026-03-12 13:10:58 +0000

    Création du service produits pour récupérer les produits depuis l'API Laravel

---
+++ src/services/productService.js
@@ -0,0 +1,15 @@
+import api from "../Api/axios";
+
+export const getProducts = (params = {}) => {
+
+  return api.get("/api/products", {
+    params
+  });
+
+};
+
+export const getProduct = (id) => {
+
+  return api.get(`/api/products/${id}`);
+
+};
