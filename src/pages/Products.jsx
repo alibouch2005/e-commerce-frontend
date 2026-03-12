4c0b32aad6e03463943e9df3ebff367b2306a23f
Ali <ali@gmail.com>
2026-03-12 13:37:37 +0000

    Implémentation de la grille produits sur la page catalogue

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getProducts } from "../services/productService";
+import ProductCard from "../components/product/ProductCard";
 
 export default function Products(){
 
@@ -9,7 +10,6 @@ useEffect(()=>{
 
   getProducts()
     .then(res=>{
-    console.log("API RESPONSE :", res.data);
       setProducts(res.data.data);
     });
 
@@ -17,14 +17,27 @@ useEffect(()=>{
 
 return(
 
-<div>
-<h1>Products</h1>
-{products.length === 0 && <p>Aucun produit</p>}
+<div className="max-w-7xl mx-auto p-6">
+
+<h1 className="text-3xl font-bold mb-6">
+Products
+</h1>
+
+{products.length === 0 && (
+
+<p className="text-gray-500">
+Aucun produit
+</p>
+
+)}
+
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+
 {products.map(product=>(
-<div key={product.id}>
-{product.name} - {product.price}
-</div>
+<ProductCard key={product.id} product={product} />
 ))}
+
+</div>
 
 </div>
 
