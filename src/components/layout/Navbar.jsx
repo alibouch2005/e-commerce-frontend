2356b601cd06712e9d14e85b106c0480a4eec0cd
Ali <ali@gmail.com>
2026-03-12 21:57:51 +0000

    Création de la Navbar dynamique avec gestion login/logout

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { logout } from "../../services/authService";
+
+export default function Navbar(){
+
+const { user,setUser } = useContext(AuthContext);
+
+const handleLogout = async ()=>{
+
+await logout();
+
+setUser(null);
+
+};
+
+return(
+
+<nav className="bg-white shadow-md">
+
+<div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+
+<div className="flex gap-6">
+
+<Link to="/" className="font-bold">
+Home
+</Link>
+
+<Link to="/products">
+Products
+</Link>
+
+</div>
+
+<div className="flex gap-4 items-center">
+
+{!user && (
+
+<>
+<Link to="/login">Login</Link>
+<Link to="/register">Register</Link>
+</>
+
+)}
+
+{user && (
+
+<>
+<span className="font-medium">
+Bonjour {user.name}
+</span>
+
+<button
+onClick={handleLogout}
+className="text-red-500"
+>
+Logout
+</button>
+</>
+
+)}
+
+</div>
+
+</div>
+
+</nav>
+
+);
+
+}
