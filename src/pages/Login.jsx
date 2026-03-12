c034f8db7cd370748fa50c918a50dac14b9094be
Ali <ali@gmail.com>
2026-03-12 13:14:26 +0000

    Ajouter useContext pour récupérer la fonction setUser du contexte d'authentification

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState , useContext} from "react";
 import { login } from "../services/authService";
 
+import { AuthContext } from "../context/AuthContext";
 import Input from "../components/ui/Input";
 import Button from "../components/ui/Button";
 
@@ -9,7 +10,8 @@ export default function Login() {
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
   const [error,setError] = useState("");
-
+  
+  const { setUser } = useContext(AuthContext);// Récupérer la fonction setUser du contexte d'authentification
   const handleSubmit = async (e) => {
 
     e.preventDefault();
@@ -19,13 +21,13 @@ export default function Login() {
       const res = await login(email,password);
 
       console.log(res.data.user);
-
+      setUser(res.data.user); // Mettre à jour l'état de l'utilisateur dans le contexte d'authentification
       alert("Connexion réussie");
 
     }catch(err){
 
       const message =
-        err?.response?.data?.message || "Erreur login";
+        err?.response?.data?.message ;
 
       console.log(message);
 
@@ -37,12 +39,13 @@ export default function Login() {
 
   return (
 
-    <div className="max-w-md mx-auto mt-20">
+    <div className="max-w-md mx-auto mt-20 p-6 bg-white rounded-lg shadow-md">
 
       <form
         onSubmit={handleSubmit}
         className="flex flex-col gap-4"
       >
+        <h1 className="text-2xl font-bold text-center">Connexion</h1>
 
         <Input
           label="Email"
