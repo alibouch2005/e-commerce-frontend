cad79aa1cb79dc712c42426c36eda693091c8e09
Ali <ali@gmail.com>
2026-03-13 15:12:02 +0000

    Ajout du composant Navbar avec bouton logout

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,70 +3,45 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { logout } from "../../services/authService";
 
-export default function Navbar(){
+export default function Navbar() {
+  const { user, setUser } = useContext(AuthContext);
 
-const { user,setUser } = useContext(AuthContext);
+  const handleLogout = async () => {
+    await logout();
 
-const handleLogout = async ()=>{
+    setUser(null);
+  };
 
-await logout();
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        <div className="flex gap-6">
+          <Link to="/" className="font-bold">
+            Home
+          </Link>
 
-setUser(null);
+          <Link to="/products">Products</Link>
+        </div>
 
-};
+        <div className="flex gap-4 items-center">
+          {!user && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
 
-return(
+          {user && (
+            <>
+              <span className="font-medium">Bonjour {user.name}</span>
 
-<nav className="bg-white shadow-md">
-
-<div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-
-<div className="flex gap-6">
-
-<Link to="/" className="font-bold">
-Home
-</Link>
-
-<Link to="/products">
-Products
-</Link>
-
-</div>
-
-<div className="flex gap-4 items-center">
-
-{!user && (
-
-<>
-<Link to="/login">Login</Link>
-<Link to="/register">Register</Link>
-</>
-
-)}
-
-{user && (
-
-<>
-<span className="font-medium">
-Bonjour {user.name}
-</span>
-
-<button
-onClick={handleLogout}
-className="text-red-500"
->
-Logout
-</button>
-</>
-
-)}
-
-</div>
-
-</div>
-
-</nav>
-
-);
-
+              <button onClick={handleLogout} className="text-red-500">
+                Logout
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
