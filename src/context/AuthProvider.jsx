ebad0d72a968ef113255f287d999d699840a5ed6
Ali <ali@gmail.com>
2026-03-13 15:13:06 +0000

    Refactorisation du AuthProvider et simplification de la gestion du logout

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,36 +2,33 @@ import { useState, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
 import { getUser, logout } from "../services/authService";
 
-
-
-
 export function AuthProvider({ children }) {
 
-  const [user,setUser] = useState(null);
-  const [loading,setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-useEffect(()=>{
+  useEffect(() => {
 
-  getUser()
-    .then(res=>{
-      setUser(res.data);
-    })
-    .catch((err)=>{
+    getUser()
+      .then(res => {
+        setUser(res.data);
+      })
+      .catch(err => {
 
-      if(err.response?.status === 401){
-        setUser(null);
-      }else{
-        console.error(err);
-      }
+        if (err.response?.status === 401) {
+          setUser(null);
+        } else {
+          console.error(err);
+        }
 
-    })
-    .finally(()=>{
-      setLoading(false);
-    });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
 
-},[])
+  }, []);
 
-  const handleLogout = async ()=>{
+  const logoutUser = async () => {
 
     await logout();
 
@@ -39,13 +36,13 @@ useEffect(()=>{
 
   };
 
-  return(
+  return (
 
     <AuthContext.Provider
       value={{
         user,
         setUser,
-        logout:handleLogout,
+        logoutUser,
         loading
       }}
     >
