100a8063faf26bdb6e9a106cadbac83976e4087a
Ali <ali@gmail.com>
2026-03-13 17:12:11 +0000

    Refactorisation de la page Products pour utiliser ProductGrid

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,46 +1,32 @@
 import { useEffect, useState } from "react";
 import { getProducts } from "../services/productService";
-import ProductCard from "../components/product/ProductCard";
+import ProductGrid from "../components/products/ProductGrid";
 
 export default function Products(){
 
-const [products,setProducts] = useState([]);
+  const [products,setProducts] = useState([]);
 
-useEffect(()=>{
+  useEffect(()=>{
 
-  getProducts()
-    .then(res=>{
-      setProducts(res.data.data);
-    });
+    getProducts()
+      .then(res=>{
+        setProducts(res.data.data);
+      });
 
-},[]);
+  },[]);
 
-return(
+  return(
 
-<div className="max-w-7xl mx-auto p-6">
+    <div className="max-w-7xl mx-auto p-6">
 
-<h1 className="text-3xl font-bold mb-6">
-Products
-</h1>
+      <h1 className="text-2xl font-bold mb-6">
+        Nos Produits
+      </h1>
 
-{products.length === 0 && (
+      <ProductGrid products={products} />
 
-<p className="text-gray-500">
-Aucun produit
-</p>
+    </div>
 
-)}
-
-<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-
-{products.map(product=>(
-<ProductCard key={product.id} product={product} />
-))}
-
-</div>
-
-</div>
-
-)
+  )
 
 }
