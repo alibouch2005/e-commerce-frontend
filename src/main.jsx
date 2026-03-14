5f68a12ef238b2ac4289af001d9369e890bce5f2
Ali <ali@gmail.com>
2026-03-14 17:17:31 +0000

    Ajout du CartProvider dans l'application React

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './context/AuthProvider.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
+import CartProvider from "./context/CartProvider";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <AuthProvider>
-
-    <App />
-
+    <CartProvider>
+      <App />
+    </CartProvider>
   </AuthProvider>,
-)
+);
