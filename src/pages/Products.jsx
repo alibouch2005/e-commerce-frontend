9f5130bf9e8e6cc9b516a8624a9d63ee2f13a20a
Ali <ali@gmail.com>
2026-03-14 13:46:02 +0000

    Ajout du filtre par catégorie dans la page catalogue des produits

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,22 +4,25 @@ import ProductGrid from "../components/products/ProductGrid";
 import ProductPagination from "../components/products/ProductPagination";
 import ProductSearch from "../components/products/ProductSearch";
 import ProductSkeletonGrid from "../components/products/ProductSkeletonGrid";
+import CategoryFilter from "../components/categories/CategoryFilter";
 
 export default function Products() {
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
-
-  const { products, lastPage, loading } = useProducts(page, search);
+  const [category,setCategory] = useState(null);
+  
+  const { products, lastPage, loading } = useProducts(page, search, category);
 
   useEffect(() => {
     setPage(1);
-  }, [search]);
+  }, [search, category]);
 
   return (
     <div className="max-w-7xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">Nos Produits</h1>
 
       <ProductSearch setSearch={setSearch} />
+      <CategoryFilter setCategory={setCategory} />
 
       {loading ? <ProductSkeletonGrid /> : <ProductGrid products={products} />}
 
