a91b281b86da8833fd5331e99454a94919bf23d9
Ali <ali@gmail.com>
2026-04-01 20:46:38 +0100

    feat(auth): ajout du composant AdminGuard pour la protection des routes

---
+++ src/guards/AdminGuard.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function AdminGuard({ children }) {
+
+  const { user } = useContext(AuthContext);
+
+  if (!user) return <Navigate to="/login" />;
+
+  if (user.role !== "admin") {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+}
